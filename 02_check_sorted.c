af4942ea61cb2720fe7c2849d4f3215d6d43a071
Sohini Ball <ballsohini939@gmail.com>
2026-09-07 12:43:06 +0530

    Add sorted array check program

---
+++ 02_check_sorted.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+
+int main()
+{
+    int n, i;
+    int arr[100];
+    int sorted = 1;
+
+    printf("Enter number of elements: ");
+    scanf("%d", &n);
+
+    printf("Enter array elements:\n");
+
+    for(i = 0; i < n; i++)
+    {
+        scanf("%d", &arr[i]);
+    }
+
+    for(i = 0; i < n - 1; i++)
+    {
+        if(arr[i] > arr[i + 1])
+        {
+            sorted = 0;
+            break;
+        }
+    }
+
+    if(sorted == 1)
+    {
+        printf("Array is sorted.");
+    }
+    else
+    {
+        printf("Array is not sorted.");
+    }
+
+    return 0;
+}
