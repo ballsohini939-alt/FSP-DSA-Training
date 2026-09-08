e419d6f8eafc294b9f83d3897bf6abd846214e93
Sohini Ball <ballsohini939@gmail.com>
2026-09-08 12:39:05 +0530

    Add function to count linked list nodes

---
+++ 13_count_nodes.c
@@ -0,0 +1,123 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+// Create a singly linked list node
+struct node
+{
+    int data;
+    struct node *next;
+};
+
+// Create a new node
+void create_newNode(struct node **head, int data)
+{
+    struct node *newNode = (struct node*)malloc(sizeof(struct node));
+
+    newNode->data = data;
+    newNode->next = NULL;
+
+    if(*head == NULL)
+    {
+        *head = newNode;
+    }
+    else
+    {
+        struct node *temp = *head;
+
+        while(temp->next != NULL)
+        {
+            temp = temp->next;
+        }
+
+        temp->next = newNode;
+    }
+}
+
+// Display linked list
+void display(struct node *head)
+{
+    struct node *temp = head;
+
+    while(temp != NULL)
+    {
+        printf("%d ", temp->data);
+        temp = temp->next;
+    }
+
+    printf("\n");
+}
+
+// Count the number of nodes
+int count_nodes(struct node *head)
+{
+    struct node *temp = head;
+    int count = 0;
+
+    while(temp != NULL)
+    {
+        count++;
+        temp = temp->next;
+    }
+
+    return count;
+}
+
+int main()
+{
+    struct node *head = NULL;
+
+    int n, data, i;
+    int choice;
+
+    printf("Enter the number of nodes: ");
+    scanf("%d", &n);
+
+    for(i = 0; i < n; i++)
+    {
+        printf("Enter data for node %d: ", i + 1);
+        scanf("%d", &data);
+
+        create_newNode(&head, data);
+    }
+
+    // Use switch case to display, create new node or count nodes
+    do
+    {
+        printf("\n1. Create new node\n");
+        printf("2. Display linked list\n");
+        printf("3. Count nodes\n");
+        printf("4. Exit\n");
+
+        printf("Enter your choice: ");
+        scanf("%d", &choice);
+
+        switch(choice)
+        {
+            case 1:
+                printf("Enter data for new node: ");
+                scanf("%d", &data);
+
+                create_newNode(&head, data);
+                break;
+
+            case 2:
+                printf("Linked list: ");
+                display(head);
+                break;
+
+            case 3:
+                printf("Number of nodes = %d\n", count_nodes(head));
+                break;
+
+            case 4:
+                printf("Exiting...\n");
+                break;
+
+            default:
+                printf("Invalid choice! Please try again.\n");
+        }
+
+    } while(choice != 4);
+
+    return 0;
+}
